629a7b95b509fd7212c7f777113df5dc398fa376
bartek <klys.bartosz@gmail.com>
2016-05-19 12:09:42 +0200

    Contracting Companies added.

---
+++ src/main/java/ContractingCompany.java
@@ -0,0 +1,29 @@
+import Entities.Payee;
+
+public class ContractingCompany implements Payee {
+    private String name;
+    private Integer bankAccount;
+    private Double currentBalance;
+    public ContractingCompany(String name, Integer bankAccount) {
+        this.name = name;
+        this.bankAccount = bankAccount;
+        currentBalance = 0.0;
+    }
+    public String name() {
+        return name;
+    }
+    public Double grossPayment() {
+        return doPayment();
+    }
+    private Double doPayment() {
+        Double balance = currentBalance;
+        currentBalance = 0.0;
+        return balance;
+    }
+    public Integer bankAccount() {
+        return bankAccount;
+    }
+    public void payForServices(Double amount) {
+        currentBalance += amount;
+    }
+}
