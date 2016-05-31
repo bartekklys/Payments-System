a46bdd118dcc7cb50260afcf882a2e0f677c53ee
bartek <klys.bartosz@gmail.com>
2016-05-31 16:07:01 +0200

    set db in dbtest class

--- src/test/java/DBTest.java
+++ src/test/java/DBTest.java
@@ -1,10 +1,20 @@
 import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 
 import java.io.FileInputStream;
 
 public class DBTest extends DBTestCase{
+
+    public DBTest(String name)
+    {
+        super( name );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.postgresql.Driver" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:postgresql://localhost:5432/payment" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "postgres" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "password" );
+    }
 
     protected IDataSet getDataSet() throws Exception {
         return new FlatXmlDataSetBuilder().build(new FileInputStream("dataset.xml"));
