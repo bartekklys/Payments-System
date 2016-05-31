27ab4978c1cc3ffe92831b1f97c086bd4ede3bfa
bartek <klys.bartosz@gmail.com>
2016-05-31 13:26:08 +0200

    fake data for salary/commission emplotee and contracting comapny, singletable strategy is done

--- src/main/java/DatabaseManager/Start.java
+++ src/main/java/DatabaseManager/Start.java
@@ -6,6 +6,7 @@ import Entities.ContractingCompany;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.jfairy.Fairy;
+import org.jfairy.producer.company.Company;
 import org.jfairy.producer.person.Person;
 
 public class Start {
@@ -17,14 +18,30 @@ public class Start {
 
         Fairy fairy = Fairy.create();
 
-        for(int i = 0 ; i < 10 ; i++) {
+        for(int i = 0 ; i < 20 ; i++) {
             Person salaryPerson = fairy.person();
-            Integer bankAccount = Integer.valueOf(fairy.baseProducer().numerify("## #### #### #### #### #### ####"));
-            Double grossWage = Double.valueOf(fairy.baseProducer().numerify("###"));
-            Double allowance = Double.valueOf(fairy.baseProducer().numerify("###"));
-            SalaryEmployee salaryEmployee = new SalaryEmployee(salaryPerson.firstName(), bankAccount, grossWage, allowance);
+            Person commissionPerson = fairy.person();
+            Company company = fairy.company();
+
+            Integer salaryBankAccount = Integer.valueOf(fairy.baseProducer().numerify("######"));
+            Integer commissionBankAccount = Integer.valueOf(fairy.baseProducer().numerify("######"));
+            Integer contractingBankAccount = Integer.valueOf(fairy.baseProducer().numerify("######"));
+
+            Double salaryGrossWage = Double.valueOf(fairy.baseProducer().numerify("####.##"));
+            Double commissionGrossWage = Double.valueOf(fairy.baseProducer().numerify("####.##"));
+
+            Double salaryAllowance = Double.valueOf(fairy.baseProducer().numerify("####.##"));
+            Double commissionAllowance = Double.valueOf(fairy.baseProducer().numerify("####.##"));
+
+            SalaryEmployee salaryEmployee = new SalaryEmployee(salaryPerson.firstName(), salaryBankAccount, salaryGrossWage, salaryAllowance);
+
+            CommissionEmployee commissionEmployee = new CommissionEmployee(commissionPerson.firstName(), commissionBankAccount, commissionGrossWage, commissionAllowance);
+
+            ContractingCompany contractingCompany = new ContractingCompany(company.name(), contractingBankAccount);
 
             session.save(salaryEmployee);
+            session.save(commissionEmployee);
+            session.save(contractingCompany);
         }
         session.getTransaction().commit();
         sessionFactory.close();
